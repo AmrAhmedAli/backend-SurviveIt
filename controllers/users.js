0993a6d9f1f9277d278d88b962e944e69d1acab9
Amr Ali <amra@ecs.eg>
2020-04-24 00:10:02 +0200

    Location service added

--- controllers/users.js
+++ controllers/users.js
@@ -50,5 +50,40 @@ router.get('/getUserByNationalID/:nationalID', function (req, res) {
 
 })
 
+router.post('/addLocation', function(req, res){
+  if(req.body.phoneNumber == null || req.body.location == null ){
+    res.send({status: 500, data: 'Please enter a body'});
+    return;
+  }
+  let usersRef = db.collection('users');
+  usersRef.where('phoneNumber', '==', req.body.phoneNumber).get()
+  .then(snapshot => {
+    if (snapshot.empty) {
+      res.send({status: 404, data:'User not found.'});
+      return;
+    }  
+
+  snapshot.forEach(doc => {
+    var items = 0;
+    req.body.location.forEach(ele =>{
+      usersRef
+      .doc(doc.id).update({
+        location: admin.firestore.FieldValue.arrayUnion(ele)
+      }).then(function(aa){
+        items++;
+        if(items === req.body.location.length){
+          res.send({status: 200});
+          return;
+        }
+        
+      })
+    });
+  });
+})
+.catch(err => {
+  res.send({status: 500, data: 'Error getting documents: '+ err});
+});
+
+})
 
 module.exports = router
